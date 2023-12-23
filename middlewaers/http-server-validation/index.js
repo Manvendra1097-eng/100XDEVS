0461d7e0ea4e72dd6246428a8a4ad7759fa96805
manvendra <manvendra1097@gmail.com>
2023-12-23 14:12:28 +0530

    adding first middlewaer code

--- middlewaers/http-server-validation/index.js
+++ middlewaers/http-server-validation/index.js
@@ -1,33 +1,39 @@
-const express = require('express')
+const express = require('express');
 
-const app = express()
+const app = express();
 
 app.use(express.json());
 
-const todos = [{
-    id:"1",
-    title:"study dsa",
-    "description":"I have to study dsa"
-}]
+const todos = [
+  {
+    id: '1',
+    title: 'study dsa',
+    description: 'I have to study dsa',
+  },
+];
 
-function validationOfId(req,res,next){
-    const {id}=req.params;
-    if(!id) return res.status(400).json({msg:"invalid id"})
-    const todo = todos.find(todo=>todo.id===id);
-   if(!todo) return res.status(404).json({msg:"todo not found"})
-   req.todo=todo;
-    next()
+function validationOfId(req, res, next) {
+  const { id } = req.params;
+  if (!id) return res.status(400).json({ msg: 'invalid id' });
+  const todo = todos.find((todo) => todo.id === id);
+  if (!todo) return res.status(404).json({ msg: 'todo not found' });
+  req.todo = todo;
+  next();
 }
 
+app.get('/:id', validationOfId, (req, res) => {
+  const todo = req.todo;
+  res.status(200).json({ todo });
+});
 
-app.get("/:id",validationOfId,(req,res)=>{
-    const todo=req.todo;
-    res.status(200).json({todo})
-})
+app.post('/', (req, res) => {
+  const todo = req.body.todo;
+  todos.push(todo);
+  res.status(200).json({ todo });
+});
 
-app.use((req,res)=>{
-    res.status(500).json({msg:"Internal server error"})
-})
+app.use((req, res) => {
+  res.status(500).json({ msg: 'Internal server error' });
+});
 
-
-app.listen(3000,()=>console.log("started at port 3000"))
+app.listen(3000, () => console.log('started at port 3000'));
