a9d4ad3398f28d520355bb32893366834a9a449e
manvendra <manvendra1097@gmail.com>
2024-01-24 23:33:16 +0530

    adding simple paytm-app

--- tailwind-prac/src/App.jsx
+++ tailwind-prac/src/App.jsx
@@ -1,27 +1,11 @@
 import './App.css';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
-    <>
-      {/* <div style={{ display: 'flex', justifyContent: 'space-around' }}>
-        <div style={{ backgroundColor: 'red' }}>Hi</div>
-        <div style={{ backgroundColor: 'green' }}>Hi</div>
-        <div style={{ backgroundColor: 'orangered' }}>Hi</div>
-      </div> */}
-
-      {/* <div className="flex justify-around">
-        <div className="bg-red-700">Hi</div>
-        <div className="bg-green-700">Hi</div>
-        <div className="bg-yellow-700">Hi</div>
-      </div> */}
-
-      <div className="grid grid-cols-2 md:grid-cols-3">
-        <div className="bg-red-700">Hi</div>
-        <div className="bg-green-700">Hi</div>
-        <div className="bg-yellow-700">Hi</div>
-        <div className="bg-purple-700">Hi</div>
-      </div>
-    </>
+    <div className="font-inter">
+      <Sidebar />
+    </div>
   );
 }
 
